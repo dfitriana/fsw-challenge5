ff1938a5b4772bff17829507fb55f13c83028929
dfitriana <diyahayufa@students.unnes.ac.id>
2022-04-20 22:44:45 +0700

    add database

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require("cors"); // inisiasi variable yang berisi cors
+const bodyParser = require("body-parser"); // inisiasi variable yang berisi body parser
+const expressLayouts = require("express-ejs-layouts");
+
 var indexRouter = require('./routes');
 // var carRouter = require('./routes/car');
 
@@ -12,12 +16,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/car', carRouter);
